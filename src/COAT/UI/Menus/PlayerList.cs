89b503432d859941f0e4337149d73f6ccda8597e
bryan-000 <fredayddd321ewq@gmail.com>
2025-05-21 07:45:25 +0100

    fix issue where it would just make another PlayerInfoMenu each time u Rebuild(); and made it so u can toggle PlayerInfoMenu (+ made it reset the toggle if its a new person)

--- src/COAT/UI/Menus/PlayerList.cs
+++ src/COAT/UI/Menus/PlayerList.cs
@@ -23,7 +23,8 @@ public class PlayerList : CanvasSingleton<PlayerList>, IMenuInterface
 {
     private RectTransform content;
     UnityEngine.UI.Image InfoMenu;
-    GameObject PlayerListGameObject;
+    Transform PlayerListTransform;
+    bool ShowPlayerInfoMenu = false;
 
     private void Start()
     {
@@ -82,7 +83,7 @@ public class PlayerList : CanvasSingleton<PlayerList>, IMenuInterface
     }
 
     /// <summary> Rebuilds the player list to add new players or remove players left the lobby. </summary>
-    public void Rebuild()
+    /*public void Rebuild()
     {
         // destroy old player list
         //if (transform.childCount > 3) Destroy(transform.GetChild(3).gameObject);
@@ -95,37 +96,29 @@ public class PlayerList : CanvasSingleton<PlayerList>, IMenuInterface
 
         content.sizeDelta = new(336f, height - 28f);
 
-        PlayerListGameObject = GameObject.Find("COAT/UI/Player List");
-        foreach (Transform child in PlayerListGameObject.transform)
-            if (child.name == "PlayerInfoMenu") Destroy(child.parent);
-
-        InfoMenu = UIB.Table("PlayerInfoMenu", transform, new(99999, 99999, 0, 0), parent =>
-        {
-            UIB.Image("PlayerInfoMenu Border", parent, new(0, 0, 120, 120), null, fill: false).maskable = false;
-        });
-        InfoMenu.maskable = false;
-
         foreach (var member in LobbyController.Lobby?.Members)
         {
             UIB.Table(
                 "Player Info", content, new(0, y -= 88, 320f, 80f, new(.5f, 1f)), player => {
                     string name = member.IsFriend ? "❤️" + member.Name : member.Name;
-                    
+
                     if (LobbyController.LastOwner == member.Id)
                         name = "★" + name;
 
                     // Load the main compodents of the UI
                     UIB.Button("", player, new(0, 0, 320f, 80f),
-                        /*Networking.GetTeam(member).Color()*/ Color.white, 24, clicked: () => SteamFriends.OpenUserOverlay(member.Id, "steamid"));
+                        /*Networking.GetTeam(member).Color()*//* Color.white, 24, clicked: () => SteamFriends.OpenUserOverlay(member.Id, "steamid"));
+
                     UnityEngine.UI.Image PFP = UIB.Image("PFP", player, new(-125, 0, 50, 50));
                     Mask PFPMASK = UIB.Mask($"PFP MASK OF {member.Name}", player, new(-125, 0, 50, 50), UIB.Background);
                     PFP.transform.SetParent(PFPMASK.transform);
+
                     UIB.Text(name, player, new(30, 12.5f, 230f, 30), align: TextAnchor.MiddleLeft);
 
                     // Add the + button
                     UIB.Button("+", player, new(135f, -15f, 30f, 30f),
-                        /*Networking.GetTeam(member).Color()*/ Color.white, 24,
-                        clicked: () => PlayerInfoMenu(member, player)); 
+                        /*Networking.GetTeam(member).Color()*//* Color.white, 24,
+                        clicked: () => PlayerInfoMenu(member, player));
 
                     // Add a ban button for the owner
                     //if (LobbyController.IsOwner && LobbyController.LastOwner != member.Id)
@@ -134,34 +127,98 @@ public class PlayerList : CanvasSingleton<PlayerList>, IMenuInterface
                     // To load in the PFPs
                     LoadPFP(member, PFP);
                 });
+        } 
+            }*/
+
+            /// <summary> Rebuilds the player list to add new players or remove players left the lobby. </summary>
+    public void Rebuild()
+    {
+        ShowPlayerInfoMenu = false;
+        // destroy old player list
+        //if (transform.childCount > 3) Destroy(transform.GetChild(3).gameObject);
+        if (content.childCount > 0) foreach (Transform child in content) Destroy(child.gameObject);
+        //if (LobbyController.Offline) return;
+
+        float height = (LobbyController.Lobby.Value.MemberCount * 88) + 24f;
+        float y = 44f;
+
+        InfoMenu = UIB.Table("PlayerInfoMenu", content, new(99999, 99999, 0, 0), parent =>
+        {
+            UIB.Image("PlayerInfoMenu Border", parent, new(0, 0, 120, 120), null, fill: false).maskable = false;
+        });
+        InfoMenu.maskable = false;
+
+        content.sizeDelta = new(336f, height - 28f);
+
+        foreach (var member in LobbyController.Lobby?.Members)
+        {
+            UIB.Table("Player Info", content, new(0, y -= 88, 320f, 80f, new(.5f, 1f)), player => 
+            {
+                string name = member.IsFriend ? "❤️" + member.Name : member.Name;
+
+                if (LobbyController.LastOwner == member.Id)
+                    name = "★" + name;
+
+                // Load the main compodents of the UI
+                UIB.Button("", player, new(0, 0, 320f, 80f), /*Networking.GetTeam(member).Color()*/ Color.white, 24, clicked: () => SteamFriends.OpenUserOverlay(member.Id, "steamid"));
+                UnityEngine.UI.Image PFP = UIB.Image("PFP", player, new(-125, 0, 50, 50));
+                Mask PFPMASK = UIB.Mask($"PFP MASK OF {member.Name}", player, new(-125, 0, 50, 50), UIB.Background);
+                PFP.transform.SetParent(PFPMASK.transform);
+                UIB.Text(name, player, new(30, 12.5f, 230f, 30), align: TextAnchor.MiddleLeft);
+
+                // Add the + button
+                UIB.Button("+", player, new(135f, -15f, 30f, 30f), /*Networking.GetTeam(member).Color()*/ Color.white, 24, clicked: () => PlayerInfoMenu(member, player, !ShowPlayerInfoMenu));
+
+                // Add a ban button for the owner
+                //if (LobbyController.IsOwner && LobbyController.LastOwner != member.Id)
+                //    UIB.IconButton("X", player, Icon(45f, 52.5f), red, clicked: () => Administration.Ban(member.Id.AccountId));
+
+                // To load in the PFPs
+                LoadPFP(member, PFP);
+            });
         }
     }
 
-    private void PlayerInfoMenu(Steamworks.Friend member, Transform parent)
+    /*private void DeletePlayerInfoMenu()
     {
-        PlayerListGameObject = GameObject.Find("COAT/UI/Player List");
-        foreach (Transform child in PlayerListGameObject.transform)
-            if (child.name == "PlayerInfoMenu") Destroy(child.parent);
-        
-        InfoMenu.transform.SetParent(parent);
-        InfoMenu.GetComponent<RectTransform>().sizeDelta = new UnityEngine.Vector2(120f, 120f);
-        InfoMenu.GetComponent<RectTransform>().anchoredPosition = new UnityEngine.Vector2(265, 0);
+        PlayerListTransform = GameObject.Find("COAT/UI/Player List").transform;
 
-        // Ban Button
-        UnityEngine.UI.Button BanButton = UIB.Button("Ban", "B", InfoMenu.transform, new(-25, 25, 45, 45), Color.red, 24, clicked: () => Administration.Ban(member.Id.AccountId));
-        BanButton.GetComponent<UnityEngine.UI.Image>().maskable = false;
-        BanButton.GetComponentInChildren<Text>().maskable = false;
+        foreach (Transform child in PlayerListTransform)
+            if (child.gameObject.name == "PlayerInfoMenu")
+                Destroy(child.gameObject);
+    }*/
 
-        // Kick Button
-        UnityEngine.UI.Button KickButton = UIB.Button("Kick", "K", InfoMenu.transform, new(25, 25, 45, 45), Pal.orange, 24, clicked: () => Administration.Kick(member.Id.AccountId));
-        KickButton.GetComponent<UnityEngine.UI.Image>().maskable = false;
-        KickButton.GetComponentInChildren<Text>().maskable = false;
+    private void PlayerInfoMenu(Steamworks.Friend member, Transform parent, bool show)
+    {
+        if (show)
+        {
+            InfoMenu.transform.SetParent(parent);
+            InfoMenu.GetComponent<RectTransform>().sizeDelta = new UnityEngine.Vector2(120f, 120f);
+            InfoMenu.GetComponent<RectTransform>().anchoredPosition = new UnityEngine.Vector2(265, 0);
 
-        // Mute Button
-        UnityEngine.UI.Button MuteButton = UIB.Button("Mute/Unmute", "Mute", InfoMenu.transform, new(0, -25, 95, 45), Color.yellow, 24, clicked: () => Administration.Mute(member.Id.AccountId, !Networking.MUTEDPLAYERS.Contains(member.Id.AccountId)));
-        MuteButton.GetComponentInChildren<Text>().text = $"{(Networking.MUTEDPLAYERS.Contains(member.Id.AccountId) ? "Unmute" : "Mute")}";
-        MuteButton.GetComponentInChildren<Text>().maskable = false;
-        MuteButton.GetComponent<UnityEngine.UI.Image>().maskable = false;
+            // Ban Button
+            UnityEngine.UI.Button BanButton = UIB.Button("Ban", "B", InfoMenu.transform, new(-25, 25, 45, 45), Color.red, 24, clicked: () => Administration.Ban(member.Id.AccountId));
+            BanButton.GetComponent<UnityEngine.UI.Image>().maskable = false;
+            BanButton.GetComponentInChildren<Text>().maskable = false;
+
+            // Kick Button
+            UnityEngine.UI.Button KickButton = UIB.Button("Kick", "K", InfoMenu.transform, new(25, 25, 45, 45), Pal.orange, 24, clicked: () => Administration.Kick(member.Id.AccountId));
+            KickButton.GetComponent<UnityEngine.UI.Image>().maskable = false;
+            KickButton.GetComponentInChildren<Text>().maskable = false;
+
+            // Mute Button
+            UnityEngine.UI.Button MuteButton = UIB.Button("Mute/Unmute", "Mute", InfoMenu.transform, new(0, -25, 95, 45), Color.yellow, 24, clicked: () => Administration.Mute(member.Id.AccountId, !Networking.MUTEDPLAYERS.Contains(member.Id.AccountId)));
+            MuteButton.GetComponentInChildren<Text>().text = $"{(Networking.MUTEDPLAYERS.Contains(member.Id.AccountId) ? "Unmute" : "Mute")}";
+            MuteButton.GetComponentInChildren<Text>().maskable = false;
+            MuteButton.GetComponent<UnityEngine.UI.Image>().maskable = false;
+        }
+        else
+        {
+            InfoMenu.transform.SetParent(parent);
+            InfoMenu.GetComponent<RectTransform>().sizeDelta = new UnityEngine.Vector2(0, 0);
+            InfoMenu.GetComponent<RectTransform>().anchoredPosition = new UnityEngine.Vector2(99999, 99999);
+        }
+        ShowPlayerInfoMenu = !ShowPlayerInfoMenu;
     }
 
     public async void LoadPFP(Friend member, UnityEngine.UI.Image PFP)
