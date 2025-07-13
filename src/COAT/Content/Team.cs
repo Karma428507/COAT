30ed468c3027e8cc517774af334e324e363674e8
bryan-000 <fredayddd321ewq@gmail.com>
2025-07-13 04:27:00 +0100

    teammmm.. yummmm..

--- src/COAT/Content/Team.cs
+++ src/COAT/Content/Team.cs
@@ -21,15 +21,15 @@ public enum Team
 /// <summary> Extension class that allows you to get team data. </summary>
 public static class TeamExtensions
 {
-    /// <summary> Returns the team color, used only in the interface. </summary>
+    /// <summary> Returns the team color, used for UI and the dolls light. </summary>
     public static Color Color(this Team team) => team switch
     {
-        Team.Yellow => new(1f, .8f, .3f),
-        Team.Red    => new(1f, .2f, .1f),
-        Team.Green  => new(0f, .9f, .4f),
-        Team.Blue   => new(0f, .5f,  1f),
-        Team.Pink   => new(1f, .4f, .8f),
-        Team.Purple => new(.7f, 0f, 1f),
+        Team.Yellow => new(1f, .8f, .3f), // #FFCC4D
+        Team.Red    => new(1f, .2f, .1f), // #FF331A
+        Team.Green  => new(0f, .9f, .4f), // #00E666
+        Team.Blue   => new(0f, .5f,  1f), // #0080FF
+        Team.Pink   => new(1f, .4f, .8f), // #FF66CC
+        Team.Purple => new(.7f, 0f, 1f),  // #B300FF
         _ => new(1f, 1f, 1f)
     };
 
